82248007fb8141a83aa32cfa5fd0025fc522a9b0
bturner1273 <32464706+bturner1273@users.noreply.github.com>
2018-02-03 11:35:09 -0500

    with statewide.xlsx

--- ReadExcel.py
+++ ReadExcel.py
@@ -1,6 +1,8 @@
 import pandas
+import xlrd
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
 
-reader = pandas.read_excel("statewide.xlsx", );
+reader = pandas.read_excel("statewide.xlsx", sheetname="sheet1");
+print("brad")
